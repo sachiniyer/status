b35d332e67846818848f3d83fa2ed2ad9eedebcb
Sachin Iyer <sachinjiyer@gmail.com>
2023-07-02 04:11:04 -0700

    ws works now

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,33 @@
 mod sites;
+use axum::{extract::ws::WebSocketUpgrade, response::Response, routing::get, Router};
 use dotenv::dotenv;
+use std::collections::HashMap;
+use std::env;
 
-#[async_std::main]
-async fn main() -> tide::Result<()> {
+#[tokio::main]
+async fn main() {
     dotenv().ok();
+    check_vars();
 
-    let mut app = tide::new();
-    app.at("/").get(sites::get_sites);
-    app.listen("127.0.0.1:8080").await?;
-    Ok(())
+    let app = Router::new().route("/ws", get(handler));
+
+    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn handler(ws: WebSocketUpgrade) -> Response {
+    ws.on_upgrade(sites::handle_ws)
+}
+
+fn check_vars() {
+    let var_map: HashMap<String, String> = env::vars().collect();
+    let sites = var_map.get("NGINX");
+    match sites {
+        Some(_) => {}
+        None => {
+            panic!("NGINX environment variable not set");
+        }
+    }
 }
--- src/sites.rs
+++ src/sites.rs
@@ -1,9 +1,13 @@
+use axum::extract::ws::WebSocket;
+use futures::future::join_all;
 use reqwest;
 use std::collections::HashMap;
 use std::env;
 use std::error::Error;
 use std::fmt;
-use tide::Request;
+use tokio::sync::mpsc;
+use tokio::task;
+use tokio::task::JoinHandle;
 
 #[derive(Debug)]
 struct NginxError {
@@ -18,14 +22,43 @@ impl fmt::Display for NginxError {
     }
 }
 
-pub async fn get_sites(mut _req: Request<()>) -> tide::Result {
-    let var_map: HashMap<String, String> = env::vars().collect();
-    let sites: Vec<String> = get_nginx(var_map.get("NGINX").unwrap().to_string()).unwrap();
-    let sites: String = sites.into_iter().collect();
-    Ok(format!("{}", sites).into())
+pub struct SiteResponse {
+    pub site: String,
+    pub status: bool,
 }
 
-#[tokio::main]
+pub async fn handle_ws(mut stream: WebSocket) {
+    let var_map: HashMap<String, String> = env::vars().collect();
+    let sites = get_nginx(var_map.get("NGINX").unwrap().to_string()).await;
+    match sites {
+        Ok(sites) => {
+            let (tx, mut rx) = mpsc::unbounded_channel();
+            let mut tasks: Vec<JoinHandle<()>> = vec![];
+            for site in sites {
+                let tx = tx.clone();
+                tasks.push(task::spawn(async move {
+                    match test_site(site).await {
+                        Ok(r) => {
+                            let _ = tx.send(r.site.into());
+                        }
+                        Err(e) => {
+                            let _ = tx.send(e.to_string().into());
+                        }
+                    }
+                }));
+            }
+            while let Some(message) = rx.recv().await {
+                let _ = stream.send(message).await;
+            }
+
+            let _ = join_all(tasks).await;
+        }
+        Err(e) => {
+            let _ = stream.send(e.to_string().into()).await;
+        }
+    }
+}
+
 async fn get_nginx(url: String) -> Result<Vec<String>, NginxError> {
     let result = reqwest::get(url.to_owned()).await;
     match result {
@@ -33,8 +66,8 @@ async fn get_nginx(url: String) -> Result<Vec<String>, NginxError> {
             reqwest::StatusCode::OK => match r.text().await {
                 Ok(v) => match parse_nginx(v).await {
                     Ok(v) => Ok(v),
-                    Err(_) => Err(NginxError {
-                        message: String::from("Could not parse nginx content"),
+                    Err(e) => Err(NginxError {
+                        message: format!("Could not parse nginx content with: {}", e),
                     }),
                 },
                 Err(_) => Err(NginxError {
@@ -57,17 +90,48 @@ async fn parse_nginx(text: String) -> Result<Vec<String>, String> {
         .into_iter()
         .map(|x| x.to_string())
         .collect();
-    for l in lines {
-        let mut words: Vec<String> = l
+
+    let mut res: Vec<String> = vec![];
+
+    for (i, l) in lines.iter().enumerate() {
+        let words: Vec<String> = l
             .split_whitespace()
             .into_iter()
             .map(|x| x.to_string())
             .collect();
-        if words.len() > 2 && words[0] == "server_name" && words[1] == "sachiniyer.com" {
-            words.remove(0);
-            words.last_mut().unwrap().pop();
-            return Ok(words);
+        if words.len() > 1 && i < lines.len() - 1 && words[0] == "listen" && words[1] == "80;" {
+            let mut words: Vec<String> = lines[i + 1]
+                .clone()
+                .split_whitespace()
+                .into_iter()
+                .map(|x| x.to_string())
+                .collect();
+            if words.len() > 1 && words[0] == "server_name" {
+                words.remove(0);
+                words.last_mut().unwrap().pop();
+                res.append(&mut words.clone())
+            }
         }
     }
-    Err("Could not parse".to_string())
+    match res.len() {
+        0 => Err("Nothing found".to_string()),
+        _ => Ok(res.iter().map(|x| "https://".to_string() + x).collect()),
+    }
+}
+
+async fn test_site(url: String) -> Result<SiteResponse, String> {
+    let result = reqwest::get(url.clone()).await;
+    match result {
+        Ok(r) => match r.status() {
+            reqwest::StatusCode::OK => Ok(SiteResponse {
+                site: url.clone(),
+                status: true,
+            }),
+            _ => Ok(SiteResponse {
+                site: url.clone(),
+                status: false,
+            }),
+        },
+        Err(_) => Err("Request Failed".to_string()),
+    }
 }
